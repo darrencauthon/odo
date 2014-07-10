77c07255270cd2dc50f25c6bdd2d4fd9792c65ec
Darren Cauthon <darren@cauthon.com>
2014-07-09 21:47:42 -0500

    Still fiddling around.

--- lib/odo.rb
+++ lib/odo.rb
@@ -4,25 +4,33 @@ require 'open-uri'
 
 module Odo
 
+  def self.extract_ref_from element
+    element['href'] || element['src']
+  end
+
   def self.stubbing_things_out options
 
     url    = options[:url]
     target = options[:target]
 
-    original = Nokogiri::HTML open(url)
+    original = Nokogiri::HTML open url
 
     files = {
-              stylesheets:      original.css('script').map { |e| e['src'] },
-              javascript_files: original.css('script').map { |e| e['src'] },
-              images:           original.css('img').map { |e| e['src'] }
+              stylesheet: original.css('link'),
+              javascript: original.css('script'),
+              image:      original.css('img')
             }
+
+    files.keys.each { |k| files[k] = files[k].map { |x| extract_ref_from x } }
     files.keys.each { |k| files[k] = files[k].select { |x| x.to_s != '' } }
 
-    files_to_download = files.map do |_, v|
-                          v.map do |x| 
+    files_to_download = files.map do |type, refs|
+                          refs.map do |ref|
                             { 
-                              source: url + '/' + x.to_s,
-                              target: target + '/' + x.to_s
+                              type:     type,
+                              original: ref,
+                              source:   "#{url}/#{ref}",
+                              target:   "#{target}/#{ref}"
                             }
                           end
                         end.flatten
