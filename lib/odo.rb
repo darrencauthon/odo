c98272e3dec5df6a3427a7301c534b1ce4a290d3
Darren Cauthon <darren@cauthon.com>
2014-07-09 21:33:18 -0500

    Stubbing some features out.

--- lib/odo.rb
+++ lib/odo.rb
@@ -1,5 +1,34 @@
 require "odo/version"
+require 'nokogiri'
+require 'open-uri'
 
 module Odo
-  # Your code goes here...
+
+  def self.stubbing_things_out options
+
+    url    = options[:url]
+    target = options[:target]
+
+    original = Nokogiri::HTML open(url)
+
+    files = {
+              stylesheets:      original.css('script').map { |e| e['src'] },
+              javascript_files: original.css('script').map { |e| e['src'] },
+              images:           original.css('img').map { |e| e['src'] }
+            }
+    files.keys.each { |k| files[k] = files[k].select { |x| x.to_s != '' } }
+
+    files_to_download = files.map do |_, v|
+                          v.map do |x| 
+                            { 
+                              source: url + '/' + x.to_s,
+                              target: target + '/' + x.to_s
+                            }
+                          end
+                        end.flatten
+
+    puts files_to_download.inspect
+
+  end
+
 end
---
+++ spec/odo_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'minitest_helper'
+
+describe Odo do
+
+  describe "stubbing things out" do
+
+    it "should do something" do
+
+      Odo.stubbing_things_out(url:    URL,
+                              target: TARGET)
+        
+    end
+
+  end
+
+end
