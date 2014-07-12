89e4d7d0a5c30d572000941d0d7b2bcabdaec6c4
Darren Cauthon <darren@cauthon.com>
2014-07-12 08:03:21 -0500

    Rename.

--- lib/odo.rb
+++ lib/odo.rb
@@ -26,7 +26,7 @@ module Odo
     files.keys.each { |k| files[k] = files[k].map { |x| extract_ref_from x } }
     files.keys.each { |k| files[k] = files[k].select { |x| x.to_s != '' } }
 
-    files_to_download = files.map do |type, refs|
+    assets = files.map do |type, refs|
                           refs.map do |ref|
                             uri = URI.parse(ref)
                             download_location = "#{target}/" + (uri.host ? "#{uri.host}#{uri.path}" : uri.path).to_s
@@ -40,16 +40,16 @@ module Odo
                           end
                         end.flatten
 
-    files_to_download.reject { |x| x[:replacement_for_original].start_with?('/') }.each do |file_to_download|
+    assets.reject { |x| x[:replacement_for_original].start_with?('/') }.each do |file_to_download|
       file_to_download[:replacement_for_original] = "/" + file_to_download[:replacement_for_original]
     end
 
-    files_to_download.select { |x| x[:replacement_for_original] == "/" }.each do |file|
+    assets.select { |x| x[:replacement_for_original] == "/" }.each do |file|
       file[:replacement_for_original] = "/" + UUID.new.generate
       file[:download_location] += "/#{file[:replacement_for_original]}".gsub("//", "/")
     end
 
-    pp files_to_download
+    pp assets
 
   end
 
