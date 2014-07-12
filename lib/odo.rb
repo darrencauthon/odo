699d34ca1c62b0da79b71498ee022560c08afc0a
Darren Cauthon <darren@cauthon.com>
2014-07-12 08:21:18 -0500

    Refactor the asset building to a separate method.

--- lib/odo.rb
+++ lib/odo.rb
@@ -1,4 +1,5 @@
 require "odo/version"
+Dir[File.dirname(__FILE__) + '/odo/*.rb'].each {|file| require file }
 require 'nokogiri'
 require 'open-uri'
 require 'pp'
@@ -6,48 +7,13 @@ require 'uuid'
 
 module Odo
 
-  def self.extract_ref_from element
-    element['href'] || element['src']
-  end
 
   def self.stubbing_things_out options
 
     url    = options[:url]
     target = options[:target]
 
-    original = Nokogiri::HTML open url
-
-    files = {
-              stylesheet: original.css('link'),
-              javascript: original.css('script'),
-              image:      original.css('img')
-            }
-
-    files.keys.each { |k| files[k] = files[k].map { |x| extract_ref_from x } }
-    files.keys.each { |k| files[k] = files[k].select { |x| x.to_s != '' } }
-
-    assets = files.map do |type, refs|
-                          refs.map do |ref|
-                            uri = URI.parse(ref)
-                            download_location = "#{target}/" + (uri.host ? "#{uri.host}#{uri.path}" : uri.path).to_s
-                            { 
-                              type:                     type,
-                              original:                 ref,
-                              source:                   uri.host ? ref : "#{url}/#{uri.path}",
-                              download_location:        download_location,
-                              replacement_for_original: uri.path.to_s
-                            }
-                          end
-                        end.flatten
-
-    assets.reject { |x| x[:replacement_for_original].start_with?('/') }.each do |file_to_download|
-      file_to_download[:replacement_for_original] = "/" + file_to_download[:replacement_for_original]
-    end
-
-    assets.select { |x| x[:replacement_for_original] == "/" }.each do |file|
-      file[:replacement_for_original] = "/" + UUID.new.generate
-      file[:download_location] += "/#{file[:replacement_for_original]}".gsub("//", "/")
-    end
+    assets = Assets.from url, target
 
     pp assets
 
